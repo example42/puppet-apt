222d6a2b0a1f82dfeae1c445fb6db526c616877b
Alessandro Franceschi <al@lab42.it>
2012-12-27 18:59:48 +0100

    apt::repository linted

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -4,18 +4,18 @@
 #
 # Usage:
 #  apt::repository { "name":
-#      url => 'repository url',
-#      distro => 'distrib name',
-#      repository => 'repository name(s)'
-#      source => false
+#    url => 'repository url',
+#    distro => 'distrib name',
+#    repository => 'repository name(s)'
+#    source => false
 #  }
 #
 # For example, to add the standard Ubuntu Lucid repository, you can use
 #
 #   apt::repository { "ubuntu":
-#      url        => "http://it.archive.ubuntu.com/ubuntu/",
-#      distro     => 'lucid',
-#      repository => 'main restricted',
+#    url    => "http://it.archive.ubuntu.com/ubuntu/",
+#    distro   => 'lucid',
+#    repository => 'main restricted',
 #   }
 # This will make the file /etc/apt/sources.list.d/ubuntu.list
 # with content:
@@ -28,40 +28,43 @@
 #   deb-src http://it.archive.ubuntu.com/ubuntu/ lucid main restricted
 #
 define apt::repository (
-    $url,
-    $distro,
-    $repository,
-    $key='',
-    $key_url='',
-    $source=false
-    ) {
-    include apt
+  $url,
+  $distro,
+  $repository,
+  $key      = '',
+  $key_url  = '',
+  $template = 'apt/repository.list.erb',
+  $source   = false,
+  $ensure   = 'present'
+  ) {
+  include apt
 
-    # Create repository file
-    file { "${name}.list":
-        path    => "/etc/apt/sources.list.d/${name}.list",
-        mode    => "644",
-        owner   => "root",
-        group   => "root",
-        require => File["/etc/apt/sources.list.d"],
-        ensure  => present,
-        content => template("apt/repository.list.erb"),
-        notify  => Exec["aptget_update"],
-    }
-      if $key {
-        case $key_url {
-            '' : {
-                exec { "aptkey_add_${key_url}":
-                    command => "gpg --recv-key ${key} ; gpg -a --export | apt-key add -",
-                    unless  => "apt-key list | grep -q ${key}",
-                }
-            }
-            default: {
-                exec { "aptkey_add_${key_url}":
-                    command => "wget -O - ${key_url} | apt-key add -",
-                    unless  => "apt-key list | grep -q ${key}",
-                }
-            }
+  # Create repository file
+  file { "${name}.list":
+    ensure  => $ensure,
+    path    => "/etc/apt/sources.list.d/${name}.list",
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => File['/etc/apt/sources.list.d'],
+    content => template($template),
+    notify  => Exec['aptget_update'],
+  }
+
+  if $key {
+    case $key_url {
+      '' : {
+        exec { "aptkey_add_${key_url}":
+          command => "gpg --recv-key ${key} ; gpg -a --export | apt-key add -",
+          unless  => "apt-key list | grep -q ${key}",
+        }
+      }
+      default: {
+        exec { "aptkey_add_${key_url}":
+          command => "wget -O - ${key_url} | apt-key add -",
+          unless  => "apt-key list | grep -q ${key}",
         }
       }
     }
+  }
+}
