16901115f6400809acef1752ccec5f2674f0a79f
Alessandro Franceschi <al@lab42.it>
2012-12-27 18:38:17 +0100

    Updated apt::key

--- manifests/key.pp
+++ manifests/key.pp
@@ -7,21 +7,32 @@
 #      url => 'key url',
 #  }
 #
+# For example, to add the standard Seat key, you can use
 #
-define apt::key ( $url="" ) {
-    
-    case $url {
-        '' : {
-            exec { "aptkey_add_${name}":
-                command => "gpg --recv-key ${name} ; gpg -a --export | apt-key add -",
-                unless  => "apt-key list | grep -q ${name}",
-            }
+#   apt::key { "952C4F3A":
+#       url => "http://debian.pgol.com/seat/seat.asc",
+#   } 
+#
+
+define apt::key ( 
+    $url='',
+    $proxy='',
+    $keyserver='',
+    $fingerprint=''
+) {
+    if $url != '' {
+        exec { "aptkey_add_${name}":
+            command => "wget -O - ${url} | apt-key add -",
+            unless  => "apt-key list | grep -q ${name}",
         }
-        default: {
-            exec { "aptkey_add_${name}":
-                command => "wget -O - ${url} | apt-key add -",
-                unless  => "apt-key list | grep -q ${name}",
-            }
+    } else {
+        exec { "aptkey_adv_${name}":
+            command => "apt-key adv --keyserver ${keyserver} --recv ${fingerprint}",
+            environment => $proxy ? {
+                ''      => "",
+                default => "http_proxy=${proxy}",
+            },
+            unless  => "apt-key list | grep -q ${name}",
         }
     }
 }
