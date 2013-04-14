b826bee082cd9ecdf3a0af215c34dab213b15d78
Alessandro Franceschi <al@lab42.it>
2013-04-14 20:11:03 +0200

    Added postgresql repo

---
+++ manifests/repo/postgresql.pp
@@ -0,0 +1,30 @@
+# = Class: apt::repo::postgresql
+#
+# This class installs the postgresql repo
+#
+class apt::repo::postgresql {
+
+  case $::operatingsystem {
+  Ubuntu: {
+    apt::repository { 'postgresql':
+      url        => 'http://apt.postgresql.org/pub/repos/apt',
+      distro     => "${::lsbdistcodename}-pgdg",
+      repository => 'main',
+      key_url    => 'https://www.postgresql.org/media/keys/ACCC4CF8.asc',
+      key        => 'ACCC4CF8'
+    }
+  }
+  Debian: {
+    apt::repository { 'postgresql':
+      url        => 'http://apt.postgresql.org/pub/repos/apt',
+      distro     => "${::lsbdistcodename}-pgdg",
+      repository => 'main',
+      key_url    => 'https://www.postgresql.org/media/keys/ACCC4CF8.asc',
+      key        => 'ACCC4CF8',
+    }
+  }
+  default: {}
+  }
+
+}
+
