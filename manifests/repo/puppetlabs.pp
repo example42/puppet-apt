7104a53a595294ca316a5d2abcc7866a03b1bba9
David Schmitt <david@dasz.at>
2013-05-25 20:38:02 +0200

    Allow overriding the distro and add dependency repo

--- manifests/repo/puppetlabs.pp
+++ manifests/repo/puppetlabs.pp
@@ -1,12 +1,20 @@
-class apt::repo::puppetlabs {
-
+class apt::repo::puppetlabs ($distro = $::lsbdistcodename, $dependencies = false) {
   apt::repository { 'puppetlabs':
     url        => 'http://apt.puppetlabs.com',
-    distro     => $::lsbdistcodename,
+    distro     => $distro,
     repository => 'main',
     # key      => '1054B7A24BD6EC30',
     key        => '4BD6EC30',
   }
 
+  if $dependencies {
+    apt::repository { 'puppetlabs-dependencies':
+      url        => 'http://apt.puppetlabs.com',
+      distro     => $distro,
+      repository => 'dependencies',
+      # key      => '1054B7A24BD6EC30',
+      key        => '4BD6EC30',
+    }
+  }
 }
 
