749aafdf6b957b73262da8cd3e1de9b5ccdf7768
Riccardo Capecchi <r.capecchi@cineca.it>
2014-11-24 23:44:08 +0100

    Refactoring of unattended-upgrade function

--- manifests/unattended_upgrade_automatic.pp
+++ manifests/unattended_upgrade_automatic.pp
@@ -6,8 +6,26 @@
 # == Parameters
 #
 # [*mail*]
-#   E-mail address to which send reports
+#   E-mail address to which send reports, default don't send emails.
+# [*mailonlyerror*]
+#   Set this value to "true" to get emails only on errors. Default
+#   is to always send a mail if mail is set.
+# [*repository*]
+#   Array of extra repository to be added for unattended-upgreades,
+#   each line will be reported on the unattended-upgrades conf file example:
+#   origin=Puppetlabs,archive=wheezy,label=Puppetlabs
+# [*blacklist*]
+#   Array of packages that you don't want to automatically update
+#   each line must contain the name of a package example:
 #
+#     - vim
+#     - mysql-server
+#     - tomcat
+# [*automaticreboot*]
+#   Boolean, default if not set is false, if set to true the system
+#   will reboot if necessarry at automatictime
+# [*automatictime*]
+#   You can set the time of the reboot, default it's 02:00
 #
 # == Examples
 #
@@ -15,7 +33,15 @@
 #  apt::unattended_upgrade_automatic { }
 #
 #
-define apt::unattended_upgrade_automatic($mail = '') {
+define apt::unattended_upgrade_automatic(
+  $mail            = '',
+  $mailonlyerror   = '',
+  $repository      = '',
+  $blacklist       = '',
+  $automaticreboot = '',
+  $automatictime   = '',
+
+) {
 
   include apt::unattended_upgrade
 
@@ -33,10 +59,17 @@ define apt::unattended_upgrade_automatic($mail = '') {
 
   case $::lsbdistid {
     'Debian','Ubuntu': {
-      apt::conf { 'unattended-upgrades':
-        ensure   => present,
-        content  => template("apt/unattended-upgrades.${::lsbdistid}.erb"),
-        priority => '50',
+      file { '50unattended-upgrades':
+        ensure  => file,
+        path    => "${apt::aptconfd_dir}/50unattended-upgrades",
+        content => template("apt/unattended-upgrades.${::lsbdistid}.erb"),
+        mode    => $apt::config_file_mode,
+        owner   => $apt::config_file_owner,
+        group   => $apt::config_file_group,
+        require => Package[$apt::package],
+        before  => Exec['aptget_update'],
+        notify  => Exec['aptget_update'],
+        audit   => $apt::manage_audit,
       }
     }
     default: {}
