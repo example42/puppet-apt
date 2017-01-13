a94b9ffd3488b7fe3c424517e81621ca4576d5b2
Mathieu Parent <math.parent@gmail.com>
2017-01-13 16:34:31 +0100

    Install apt-transport-https when needed

--- manifests/repo/gocd.pp
+++ manifests/repo/gocd.pp
@@ -1,7 +1,7 @@
 # = Class: apt::repo::gocd
 #
 class apt::repo::gocd {
-
+  ensure_packages('apt-transport-https')
   apt::repository { 'gocd':
     url        => 'https://download.go.cd',
     repository => '/',
