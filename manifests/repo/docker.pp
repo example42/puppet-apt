bd932e13efe41e5a7cc686894e035f481325b04c
Mathieu Parent <math.parent@gmail.com>
2017-07-12 11:17:36 +0200

    Update and rename Docker repo

---
+++ manifests/repo/docker.pp
@@ -0,0 +1,35 @@
+# = Class: apt::repo::docker
+#
+# This class installs the Docker repo
+#
+class apt::repo::docker {
+  ensure_packages('apt-transport-https')
+  case $::operatingsystem {
+  'Debian': {
+    apt::source { 'docker':
+      location => 'https://download.docker.com/linux/debian',
+      release  => $::lsbdistcodename,
+      repos    => 'stable',
+      key      => {
+        # From https://download.docker.com/linux/debian/gpg
+        'id'      => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
+        'content' => file('apt/repo/docker.gpg'),
+      },
+    }
+  }
+  'Ubuntu': {
+    apt::source { 'docker':
+      location => 'https://download.docker.com/linux/ubuntu',
+      release  => $::lsbdistcodename,
+      repos    => 'stable',
+      key      => {
+        # From https://download.docker.com/linux/debian/gpg
+        'id'      => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
+        'content' => file('apt/repo/docker.gpg'),
+      },
+    }
+  }
+  default: {}
+  }
+
+}
