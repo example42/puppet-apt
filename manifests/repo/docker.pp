c4429cad2eebb5b398a806ce89038eedba69ce6b
Mathieu Parent <Mathieu.PARENT@nantesmetropole.fr>
2017-10-18 15:19:14 +0200

    Allow to change docker repo
    Available: stable, edge

--- manifests/repo/docker.pp
+++ manifests/repo/docker.pp
@@ -2,14 +2,16 @@
 #
 # This class installs the Docker repo
 #
-class apt::repo::docker {
+class apt::repo::docker(
+  $repos = 'stable',
+) {
   ensure_packages('apt-transport-https')
   case $::operatingsystem {
   'Debian': {
     apt::source { 'docker':
       location => 'https://download.docker.com/linux/debian',
       release  => $::lsbdistcodename,
-      repos    => 'stable',
+      repos    => $repos,
       key      => {
         # From https://download.docker.com/linux/debian/gpg
         'id'      => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
@@ -21,7 +23,7 @@ class apt::repo::docker {
     apt::source { 'docker':
       location => 'https://download.docker.com/linux/ubuntu',
       release  => $::lsbdistcodename,
-      repos    => 'stable',
+      repos    => $repos,
       key      => {
         # From https://download.docker.com/linux/debian/gpg
         'id'      => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
